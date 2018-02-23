696a297957a872020d05912b33019c0c28222dc2
Tiffany Thompson <tdthompson899@gmail.com>
2018-02-22 19:13:50 -0600

    gets random quote and background from the db and displays the quote. WIP - work on displaying the background everytime

--- server.js
+++ server.js
@@ -16,7 +16,35 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public/"));
 
 app.get('/', function (req, res) {
-    res.render('pages/index');
+    connection.query('Select * from quotes', function(err, results){
+      if (err) throw err; 
+
+      // pick a random quote
+      var randomQuote = Math.floor(Math.random() * results.length) + 1; 
+
+      quote = results[randomQuote];
+
+      console.log("Quote: ", quote);
+
+      connection.query('select * from backgrounds',  function(err, bkgnd){
+        if (err) throw err; 
+
+        // pick a random background
+        var randomBackground = Math.floor(Math.random() * bkgnd.length) + 1; 
+        console.log("Random background" + randomBackground);
+        background = bkgnd[randomBackground]; 
+        console.log("Img: ", bkgnd[randomBackground]);
+
+        // pass the quote and background to the index page
+        res.render('pages/index', {
+            'quote': quote,
+            'background': background
+        });
+
+      });
+
+    });
+
 });
 
 app.listen(port, function(){
